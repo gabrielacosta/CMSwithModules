f511085bbe0d6d4361d0b7aa1b495a25cd4d78c3
Juan Acosta <gacostab@hotmail.com>
2021-03-26 13:48:06 +1100

    Add project files.

---
+++ CMSLanding/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+#if (UseNLog)
+using OrchardCore.Logging;
+#endif
+#if (UseSerilog)
+using Serilog;
+#endif
+
+namespace CMSLanding
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => BuildHost(args).RunAsync();
+
+        public static IHost BuildHost(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+#if (UseSerilog)
+                .UseSerilog((hostingContext, configBuilder) =>
+                    {
+                        configBuilder.ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.FromLogContext();
+                    })
+#endif
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+#if (UseNLog)
+                    .UseNLogWeb()
+#endif
+                    .UseStartup<Startup>()
+                ).Build();
+    }
+
+    /*
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+*/
+}
